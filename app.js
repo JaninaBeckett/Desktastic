92180e3c7a95d4490857b14796b43b852cf998d8
Magnus Luttmann <magnus.luttmann@ibm.com>
2019-11-12 15:36:15 +0100

    Added site/ api /placeOverview

--- app.js
+++ app.js
@@ -6,37 +6,36 @@
 
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
-var express = require('express');
+var express = require("express");
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
-const bodyParser = require('body-parser');
-
+var cfenv = require("cfenv");
+const bodyParser = require("body-parser");
 
 // create a new express server
 var app = express();
 
-
 // serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser());
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
-app.get('/', (req, res) => {
-  res.send("Hello to this website")
-})
+app.get("/", (req, res) => {
+  res.send("Hello to this website");
+});
 
-app.post('/updateSeat', (req,res)=> {
+app.post("/updateSeat", (req, res) => {
   console.log(req.body);
   res.send("thanks for using the service");
-})
+});
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-
-	// print a message when the server starts listening
+app.listen(appEnv.port, "0.0.0.0", function() {
+  // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
